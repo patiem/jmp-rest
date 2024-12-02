4829fb6caf69bcce7ba4fe07068aa38332318ea1
patiem <pp.mikulska@gmail.com>
2024-12-02 10:31:14 +0100

    Init

---
+++ src/main/java/com/epa/m/multi/jmprest/JmpRestApplication.java
@@ -0,0 +1,13 @@
+package com.epa.m.multi.jmprest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmpRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JmpRestApplication.class, args);
+    }
+
+}
